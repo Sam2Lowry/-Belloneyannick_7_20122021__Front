0aebe4fdd0e1144b098ad6e577ba68bfdfbed826
Sam2Lowry <yannick.bellone@outlook.fr>
2022-02-23 10:26:03 +0100

    cardlist

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card-list',
   templateUrl: './card-list.component.html',
-  styleUrls: ['./card-list.component.scss']
+  styleUrls: ['./card-list.component.scss'],
 })
 export class CardListComponent implements OnInit {
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
