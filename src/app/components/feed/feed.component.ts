7c5feacdcdc1beb731220082f19116511b9025e4
Sam2Lowry <yannick.bellone@outlook.fr>
2022-03-25 14:56:18 +0100

    snackbar psot

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ApiService } from './../../auth/api.service';
 import { Post } from './../../models/post';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-feed',
@@ -16,20 +17,21 @@ export class FeedComponent implements OnInit {
     this.hidden = !this.hidden;
   }
 
-  constructor(private apiService: ApiService) {}
+  constructor(private apiService: ApiService, private _snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
     this.getAllPost();
     //this.getCounterComment();
   }
 
+  // Récupération de tous les posts [A paginer par la suite]
   getAllPost(): void {
     this.apiService.getAllPosts().subscribe((res) => {
       this.posts = res;
       console.log(this.posts);
     });
   }
-
+  // Récupération du nombre de commentaires d'un post
   /*getCounterComment(): void {
     this.apiService.getAllPosts().subscribe((res) => {
       this.posts = res;
